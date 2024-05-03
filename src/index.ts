abc2833e510dfe068adfc510461098cb917acf70
Jacob Harvey <jharvey@mahalobanking.com>
2024-05-03 09:49:21 -0600

    feat: add analyzer and summary classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { MatchResult } from "./MatchResult";
 import {MatchReader} from "./MatchReader";
 import {CsvFileReader} from "./CsvFileReader";
 
@@ -7,14 +6,3 @@ const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
 matchReader.load()
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
